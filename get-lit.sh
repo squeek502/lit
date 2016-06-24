4fc86b38d9fe1d958dc91c14fb9dc0fad25c61fd
squeek <squeek502@hotmail.com>
2016-06-24 03:46:10 -0700

    No s

--- get-lit.sh
+++ get-lit.sh
@@ -5,7 +5,7 @@ LIT_VERSION=3.4.1
 
 LUVI_ARCH=`uname -s`_`uname -m`
 LUVI_URL="https://github.com/luvit/luvi/releases/download/v$LUVI_VERSION/luvi-regular-$LUVI_ARCH"
-LIT_URL="https://www.ryanliptak.com/misc/lit.zip"
+LIT_URL="http://www.ryanliptak.com/misc/lit.zip"
 
 # Download Files
 echo "Downloading $LUVI_URL to luvi"
